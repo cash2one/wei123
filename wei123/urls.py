45a3348b8299927388ae4153326adbf86dbe38cf
cnwarden <cnwarden@gmail.com>
2015-01-22 11:31:40 +0800

    release to xierqi.net

--- wei123/urls.py
+++ wei123/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^weixin/admin/', include(admin.site.urls)),
     url(r'^weixin/', include('weisite.urls')),
 )
